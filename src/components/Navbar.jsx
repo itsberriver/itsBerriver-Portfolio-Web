7e17e039ffdaa813716a3476b5c314ca0bac23b4
itsberriver <jmejiaos713@gmail.com>
2022-11-14 18:25:19 +0100

    styled navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,42 @@
-import ICONS from '../media/ICONS.png'
-import { FaBars} from 'react-icons/fa';
+import logo from '../media/logo.png'
+import { FaBars,FaTimes} from 'react-icons/fa';
+import React,{useState} from 'react';
 
 function Navbar() {
+
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav (!nav)
+
     return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4  text-gray-300'>
+    <div className ='fixed w-full h-[80px] flex justify-between items-center px-4 text-primaryColor' id = 'primaryFont'>
+         {/* HAMBURGER */}
+        <div onClick = {handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars/> : <FaTimes/> }
+        </div>
+
         <div>
-            <img src={ICONS} alt="logo" />
+            <img src={logo} alt="logo" className=' h-[80px] flex justify-between items-center px-4' />
         </div>
         
-         {/* HAMBURGER */}
-        <div>
-            <FaBars />
-        </div>
 
          {/* MENU */}
-        <ul className=''>
-            <li>sobre mi</li>
-            <li>proyectos</li>
-            <li>contacta</li>
-        </ul>
+            <ul className='hidden md:flex'>
+                <li>proyectos</li>
+                <li>sobre mi</li>
+                <li>contacta</li>
+            </ul>
 
          {/* MOBILE MENU */}
-        <ul>    </ul>
+        <ul className={!nav ? 'hidden' : ' absolute top-0 left-0 w-full h-screen bg-secondaryColor flex flex-col justify-center items-center'}>    
+            <li className='py-6 text-4xl'> proyectos </li>
+            <li className='py-6 text-4xl'> sobre mi </li>
+            <li className='py-6 text-4xl'> contacta </li>
+        </ul>
+
+        {/* SOCIAL ICONS */} 
+        <div className="hidden">
+
+        </div>
 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    colors:{
+      primaryColor: '#212936',
+      secondaryColor: '#F5CAC3',
+    },
     extend: {},
   },
   plugins: [],
